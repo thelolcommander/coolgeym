bb17b90088554fb8ad3823e2bf0b6c2a6b4c6cc4
thelolcommander <48478823+thelolcommander@users.noreply.github.com>
2022-11-22 20:47:40 +0800

    newest ver
    polp

--- coolgeym/coolgeym.py
+++ coolgeym/coolgeym.py
@@ -1,38 +1,82 @@
 import turtle
-# turtle is like pygame with less steps
-import tkinter as tkinter
-from tkinter import *
-from tkinter import font
-# tkinter is a simple GUI toolkit
+from turtle import Screen, showturtle, hideturtle
+import tkinter
+
+import sys
+import winsound
 import time
 
 window = turtle.Screen()
 window.title("Sean gaming")
+tkinter_canvas = window.getcanvas()
+
+obunga_Canvas = window.getcanvas()
 # setting a variable "window" and making it a screen
 
 Wd = 800
 Hg = 800
 window.setup(Wd, Hg)
+
 #setup window screen to programmers choice of size
 
+turtle.bgcolor('black')
 window.bgpic("bgpic.gif")
 # adding a pic to the window
 # note it should be .gif or it wont work
 
+power = 101
+task_Quota = 0
+anger = 0
+
+toggle_State = True
+
+# ----- functions -----
+
 def press():
-    do_stuff()
+    if toggle_State == False:
+        show()
+        task_Label.pack_forget()
 
-def do_stuff():
-    exit()
-# basically we are making our own functions so when we call press or do_stuff its gonna call whats indented on it
+    elif toggle_State == True:
+        hide()
+        task_Label.pack()
+        task_Progress()
 
-tkinter_canvas = window.getcanvas()
+
+def show():
+    global toggle_State
+    toggle_State = True
+    tablet.hideturtle()
+    winsound.PlaySound('tablet_Close.wav', winsound.SND_ASYNC)
+    tkinter_canvas.after(1)
+    anger = anger + 1
+
+def hide():
+    global toggle_State
+    toggle_State = False
+    tablet.showturtle()
+    winsound.PlaySound('tablet_Open.wav', winsound.SND_ASYNC)
+
+def lose():
+    sys.exit()
+
+# ----- tablet -----
+turtle_Screen = Screen()
+
+obunga = turtle.Turtle()
+turtle_Screen.addshape('obunga.gif')
+obunga.shape('obunga.gif')
+
+tablet = turtle.Turtle()
+turtle_Screen.addshape('tabletpic.gif')
+tablet.shape('tabletpic.gif')
+tablet.hideturtle()
+
 tkinter_button = tkinter.Button(tkinter_canvas.master, text = "Tablet", command = press,height = 1,width = 10, font = ("Arial", 35))
 tkinter_canvas.create_window(-10, Hg/3, window = tkinter_button)
-# basically the button itself, we first tried to link up tkinter and turtle on the first line of this block then 
-# made a button and created a window for it inside the turtle screen
 
-power = 101
+
+# ----- power level -----
 
 power_Level = tkinter.Label(tkinter_canvas.master, text = "Power Level: {} ".format(power), font = ("Arial", 15))
 tkinter_canvas.create_window(-300, -350, window = power_Level)
@@ -40,16 +84,35 @@ tkinter_canvas.create_window(-300, -350, window = power_Level)
 def power_Level_Label():
     global power
     if power >= 1:
-      power = power - 1
-      power_Level.after(2000, power_Level_Label)
-      power_Level.config(text = "Power Level: {} ".format(power))
-      power_Level.update()
+         power = power - 1
+         power_Level.after(4000, power_Level_Label)
+         power_Level.config(text = "Power Level: {} ".format(power))
+         power_Level.update()
 
+    elif power < 1:
+        lose("L you lost")
 
 power_Level_Label()
 
 
+# ----- task -----
 
-# from line 35 to line 43 is one of the hardest things i had to make that would be usually easy when using a game engine, so basically i declared a power variable and then basically in line 37 and 38 i created the actually label. then i made a function that loops itself for 1000ms and then runs again then updates itself after running, next i would make a function that 
+
+task_Label = tkinter.Label(tkinter_canvas.master, text = "progress: {}/100 ".format(task_Quota), font = ("Arial", 15))
+tkinter_canvas.create_window(-1000, -1000, window = task_Label, state = "disabled")
+
+def task_Progress():
+    global task_Quota
+    while toggle_State == False:
+        if task_Quota <= 99:
+            task_Quota = task_Quota + 1
+            task_Label.config(text = "progress: {}/100 ".format(task_Quota))
+            task_Label.after(500)
+            task_Label.update()
+        
+# ----- mosnter -----
+
+if anger > 10:
+    lose()
 
 turtle.done()
